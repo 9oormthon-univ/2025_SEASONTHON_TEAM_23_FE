bc35dd94e667cb0d0c3a037aa56522fce84817f1
kmj <myng25@naver.com>
2025-08-30 10:08:47 +0900

    feat: 버튼 컴포넌트 배치

--- src/screens/letter/LetterScreen.tsx
+++ src/screens/letter/LetterScreen.tsx
@@ -1,9 +1,11 @@
 import { View, Text } from 'react-native';
+import LetterWriteButton from '@/components/letter/LetterWriteButton'; 
 
 const LetterScreen = () => {
   return (
     <View className="flex-1 items-center justify-center bg-white">
       <Text className="text-xl font-bold">한마디 편지</Text>
+      <LetterWriteButton />
     </View>
   );
 };
