e072f31145c75a7a4115bf7371ff1f3f804eeba4
cjiyun <chlwldbs78@gmail.com>
2025-09-01 00:31:35 +0900

    feat: 온보딩 페이지 구현 및 로그인 핸들러 훅 구현/분리

--- src/screens/onboarding/OnboardingScreen.tsx
+++ src/screens/onboarding/OnboardingScreen.tsx
@@ -1,25 +1,31 @@
-import { useAuth } from '@/provider/AuthProvider';
-import { Pressable, View, Text } from 'react-native';
-import { user } from '@/mocks/auth';
+import { Pressable, View, Image, Text } from 'react-native';
+import { SafeAreaView } from 'react-native-safe-area-context';
+import IcKakaoLogo from '@icons/ic-kakao-logo.svg';
+import { useKakaoLogin } from '@/hooks/useKakaoLogin';
 
 const OnboardingScreen = () => {
-  const { setUser } = useAuth();
-
-  const onKakaoPress = async () => {
-    // await login();
-    setUser(user);
-  };
+  const { loading, onKakaoPress } = useKakaoLogin();
 
   return (
-    <View className="flex-1 items-center justify-center">
-      <Pressable
-        onPress={onKakaoPress}
-        android_ripple={{ color: 'rgba(0,0,0,0.06)' }}
-        className=" rounded-[12px] bg-[#fee500] px-8 py-4"
-      >
-        <Text>카카오톡으로 로그인</Text>
-      </Pressable>
-    </View>
+    <SafeAreaView className="flex-1 items-center gap-[95px] px-7 pt-7">
+      <View className="flex-col gap-[52px]">
+        <Image source={require('@images/img-onboarding-ex.png')} />
+        <View className="flex items-center gap-3">
+          <View className="flex w-full gap-5">
+            <Pressable
+              onPress={onKakaoPress}
+              disabled={loading}
+              android_ripple={{ color: 'rgba(0,0,0,0.06)' }}
+              className="flex-row items-center justify-center gap-4 rounded-[20px] bg-[#FAE301] py-4 color-[#391B1B]"
+            >
+              <IcKakaoLogo />
+              <Text className="subHeading3">카카오 로그인</Text>
+            </Pressable>
+          </View>
+        </View>
+      </View>
+      <Text className="captionB color-gray-300">{`Developed By Team Petfarewell`}</Text>
+    </SafeAreaView>
   );
 };
 
