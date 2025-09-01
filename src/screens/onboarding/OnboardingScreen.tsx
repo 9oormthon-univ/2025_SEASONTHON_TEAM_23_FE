9e4732d5616a051d58a4514e64a299446315289b
cjiyun <chlwldbs78@gmail.com>
2025-09-01 19:27:30 +0900

    style: 온보딩 화면 UI 수정

--- src/screens/onboarding/OnboardingScreen.tsx
+++ src/screens/onboarding/OnboardingScreen.tsx
@@ -7,24 +7,26 @@ const OnboardingScreen = () => {
   const { loading, onKakaoPress } = useKakaoLogin();
 
   return (
-    <SafeAreaView className="flex-1 items-center gap-[95px] px-7 pt-7">
-      <View className="flex-col gap-[52px]">
-        <Image source={require('@images/img-onboarding-ex.png')} />
-        <View className="flex items-center gap-3">
-          <View className="flex w-full gap-5">
-            <Pressable
-              onPress={onKakaoPress}
-              disabled={loading}
-              android_ripple={{ color: 'rgba(0,0,0,0.06)' }}
-              className="flex-row items-center justify-center gap-4 rounded-[20px] bg-[#FAE301] py-4 color-[#391B1B]"
-            >
-              <IcKakaoLogo />
-              <Text className="subHeading3">카카오 로그인</Text>
-            </Pressable>
+    <SafeAreaView edges={['top']} className="bg-gray-50">
+      <View className="flex items-center gap-[95px]">
+        <View className="flex-col gap-[52px] px-7 pt-9">
+          <Image source={require('@images/img-onboarding.png')} />
+          <View className="flex items-center gap-3">
+            <View className="flex w-full gap-5">
+              <Pressable
+                onPress={onKakaoPress}
+                disabled={loading}
+                android_ripple={{ color: 'rgba(0,0,0,0.06)' }}
+                className="flex-row items-center justify-center gap-4 rounded-[20px] bg-[#FAE301] py-4 color-[#391B1B]"
+              >
+                <IcKakaoLogo />
+                <Text className="subHeading3">카카오 로그인</Text>
+              </Pressable>
+            </View>
           </View>
         </View>
+        <Text className="captionB color-gray-300">{`Developed By Team Petfarewell`}</Text>
       </View>
-      <Text className="captionB color-gray-300">{`Developed By Team Petfarewell`}</Text>
     </SafeAreaView>
   );
 };
