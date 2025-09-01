0bc78f3442348455441855225bc0ac0e1808530d
cjiyun <chlwldbs78@gmail.com>
2025-09-02 04:42:08 +0900

    style: 오늘의 일기 메인 화면 UI 구현

---
+++ src/components/diary/CustomCalendarHeader.tsx
@@ -0,0 +1,32 @@
+import { View, Text } from 'react-native';
+import Icon from '@common/Icon';
+
+type CalenderHeaderProps = {
+  month: string;
+};
+
+const DAY_NAMES = ['일', '월', '화', '수', '목', '금', '토'];
+
+const CustomCalendarHeader = ({ month }: CalenderHeaderProps) => {
+  return (
+    <View className="h-[84px] items-center justify-center gap-1 bg-white">
+      <View className="w-[183px] flex-row items-center justify-between gap-9 px-3 py-2">
+        <Icon name="IcBack" size={28} color="#9D9D9D" />
+        <Text className="subHeading1B text-gray-700">{month}월</Text>
+        <Icon name="IcNext" size={28} color="#9D9D9D" />
+      </View>
+      <View className="w-full flex-row justify-evenly gap-5 px-3 py-2">
+        {DAY_NAMES.map((n, i) => (
+          <Text
+            key={n}
+            className={`subHeading3 h-6 w-6 text-center ${i === 0 ? 'text-[#CD8383]' : i === 6 ? 'text-[#71ACC9]' : 'text-[#585858]'}`}
+          >
+            {n}
+          </Text>
+        ))}
+      </View>
+    </View>
+  );
+};
+
+export default CustomCalendarHeader;
