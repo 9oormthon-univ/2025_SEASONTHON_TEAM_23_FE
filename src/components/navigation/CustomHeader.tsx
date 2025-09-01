511177406e20d1383cc232cfb6703aa207bb03f7
cjiyun <chlwldbs78@gmail.com>
2025-09-02 05:34:42 +0900

    refactor: 커스텀 해더 구조 개선

--- src/components/navigation/CustomHeader.tsx
+++ src/components/navigation/CustomHeader.tsx
@@ -1,16 +1,19 @@
 import { SafeAreaView } from 'react-native-safe-area-context';
-import { Text, View } from 'react-native';
+import { Pressable, Text, View } from 'react-native';
 import Icon from '@common/Icon';
 import * as Icons from '@icons';
+import SaveButton from '@common/SaveButton';
 
 type HeaderProps = {
   hasBack?: boolean;
   hasLogo?: boolean;
+  hasButton?: boolean;
   icon?: keyof typeof Icons;
   title?: string;
+  onPress?: () => void;
 };
 
-const CustomHeader = ({ hasBack, hasLogo, icon, title }: HeaderProps) => {
+const CustomHeader = ({ hasBack, hasLogo, icon, hasButton, title, onPress }: HeaderProps) => {
   return (
     <SafeAreaView edges={['top']} className="bg-white">
       <View className="flex-row items-center justify-between px-7 py-4">
@@ -19,7 +22,14 @@ const CustomHeader = ({ hasBack, hasLogo, icon, title }: HeaderProps) => {
           {hasLogo && <Icon name="IcBrandLogo" width={94} height={30} />}
           {title && <Text className="subHeading2B color-gray-900">{title}</Text>}
         </View>
-        {icon && <Icon name={icon} size={32} color="#131313" />}
+        {hasButton &&
+          (icon ? (
+            <Pressable onPress={onPress}>
+              <Icon name={icon} size={32} color="#131313" />
+            </Pressable>
+          ) : (
+            <SaveButton onPress={onPress} />
+          ))}
       </View>
     </SafeAreaView>
   );
