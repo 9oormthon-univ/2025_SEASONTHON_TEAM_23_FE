b8b8f317d55c38bb6980818177245e3014b2714d
Kim Minju <myng25@naver.com>
2025-09-02 23:17:32 +0900

    refactor: 스네이크 케이스 참조를 카멜케이스로 변경

--- src/screens/letter/LetterScreen.tsx
+++ src/screens/letter/LetterScreen.tsx
@@ -1,13 +1,11 @@
 import { View } from 'react-native';
 import { LetterProvider } from '@/components/letter/LetterContext';
-import MyLettersButton from '@/components/letter/MyLettersButton';
 import LetterFeed from '@/components/letter/LetterFeed';
 
 const LetterScreen = () => {
   return (
     <LetterProvider>
       <View className="flex-1 bg-white">
-        <MyLettersButton />
         <LetterFeed />
       </View>
     </LetterProvider>
