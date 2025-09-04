e91a71635236463cc884f07d43c85bf7a126c4e4
cjiyun <chlwldbs78@gmail.com>
2025-09-05 02:20:06 +0900

    style: 삭제 알람 모달 UI 수정

--- src/components/common/ConfirmDeleteModal.tsx
+++ src/components/common/ConfirmDeleteModal.tsx
@@ -1,5 +1,6 @@
 import { Modal, View, Text, Pressable } from 'react-native';
 import Icon from '@common/Icon';
+import { keepAllKorean } from '@/utils/keepAll';
 
 type ConfirmDeleteModalProps = {
   visible: boolean;
@@ -10,7 +11,7 @@ type ConfirmDeleteModalProps = {
 
 const ConfirmDeleteModal = ({
   visible,
-  helperText = '글을 삭제하시겠습니까?',
+  helperText = '글을 삭제하면 되돌릴 수 없어요.',
   onCancel,
   onConfirm,
 }: ConfirmDeleteModalProps) => {
@@ -18,30 +19,34 @@ const ConfirmDeleteModal = ({
 
   return (
     <Modal visible transparent animationType="fade" statusBarTranslucent onRequestClose={onCancel}>
-      <View className="flex-1 items-center justify-center" style={{ backgroundColor: '#0000004D' }}>
-        <View className="rounded-4xl gap-6 bg-white px-11 py-6">
+      <View
+        className="flex-1 items-center justify-center px-[38px]"
+        style={{ backgroundColor: '#0000004D' }}
+      >
+        <View className="items-center gap-6 rounded-[32px] bg-white px-11 py-6">
           <View className="items-center gap-2">
             <Icon name="IcWarning" size={40} color="#FF2E45" />
             <View className="items-center gap-1">
-              <Text className="heading3 text-gray-900">{`글을 삭제하시겠습니까?`}</Text>
-              <Text className="body1 text-center !leading-6 text-[#343434]">{helperText}</Text>
+              <Text className="heading3 leading-9 text-gray-900">{`글을 삭제하시겠습니까?`}</Text>
+              <Text className="body1 text-center !leading-6 text-[#343434]">
+                {keepAllKorean(helperText)}
+              </Text>
             </View>
           </View>
           <View className="flex-row gap-6">
             <Pressable
               onPress={onConfirm}
               android_ripple={{ color: 'rgba(0,0,0,0.06)' }}
-              className="items-center justify-center rounded-xl border bg-error px-6 py-3"
+              className="items-center justify-center rounded-xl bg-error px-6 py-3"
             >
               <Text className="body1 !leading-6 text-white">{`삭제하기`}</Text>
             </Pressable>
-
             <Pressable
               onPress={onCancel}
               android_ripple={{ color: 'rgba(0,0,0,0.06)' }}
-              className="items-center justify-center rounded-xl border bg-gray-500 px-6 py-3"
+              className="items-center justify-center rounded-xl bg-gray-500 px-6 py-3"
             >
-              <Text className="body1 !leading-6 text-white">삭제</Text>
+              <Text className="body1 !leading-6 text-white">취소하기</Text>
             </Pressable>
           </View>
         </View>
