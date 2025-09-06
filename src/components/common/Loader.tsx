c23fe1292bc6d69d3592745a21ae7653b588d273
cjiyun <chlwldbs78@gmail.com>
2025-09-06 13:18:25 +0900

    style: 페이지 로딩일 때 스피너 위치 조정

--- src/components/common/Loader.tsx
+++ src/components/common/Loader.tsx
@@ -1,10 +1,20 @@
 import { View, ActivityIndicator } from 'react-native';
+import { SafeAreaView } from 'react-native-safe-area-context';
 
-const Loader = () => {
+type LoaderProps = {
+  isPageLoader?: boolean;
+};
+
+const Loader = ({ isPageLoader = false }: LoaderProps) => {
   return (
-    <View className="absolute bottom-0 left-0 right-0 top-0 justify-center" pointerEvents="auto">
-      <ActivityIndicator size="large" color="#FFD86F" />
-    </View>
+    <SafeAreaView
+      edges={isPageLoader ? ['top'] : []}
+      className={`absolute bottom-0 left-0 right-0 ${isPageLoader ? 'top-[60px]' : 'top-0'} z-10 justify-center bg-bg/30`}
+    >
+      <View className="" pointerEvents="auto">
+        <ActivityIndicator size="large" color="#FFD86F" />
+      </View>
+    </SafeAreaView>
   );
 };
 
