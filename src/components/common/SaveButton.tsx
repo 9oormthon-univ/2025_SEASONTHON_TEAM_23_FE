4214c988432dae8b1888f2d65567307d25b5811f
cjiyun <chlwldbs78@gmail.com>
2025-09-06 17:05:58 +0900

    feat: pull develop rebase 충돌 해결

--- src/components/common/SaveButton.tsx
+++ src/components/common/SaveButton.tsx
@@ -6,8 +6,8 @@ type SaveButtonProps = {
 
 const SaveButton = ({ onPress }: SaveButtonProps) => {
   return (
-    <Pressable onPress={onPress} className="rounded-lg bg-primary px-3 py-2">
-      <Text className="body2 text-white">{`완료`}</Text>
+    <Pressable onPress={onPress} className="rounded-lg bg-yellow-200 px-3 py-2">
+      <Text className="body2 text-gray-900">{`완료`}</Text>
     </Pressable>
   );
 };
