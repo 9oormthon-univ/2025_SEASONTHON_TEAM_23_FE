eb127743afff31114c8803089e833f67e73ff615
cjiyun <chlwldbs78@gmail.com>
2025-09-06 09:32:23 +0900

    style: 캘린더 UI 수정

--- src/components/diary/CustomCalendarHeader.tsx
+++ src/components/diary/CustomCalendarHeader.tsx
@@ -12,21 +12,21 @@ const DAY_NAMES = ['일', '월', '화', '수', '목', '금', '토'];
 
 const CustomCalendarHeader = ({ month, onPrev, onNext }: CalenderHeaderProps) => {
   return (
-    <View className="h-[84px] items-center justify-center gap-1 bg-white">
+    <View className="h-[84px] items-center justify-center gap-1">
       <View className="w-[183px] flex-row items-center justify-between gap-9 px-3 py-2">
         <Pressable onPress={onPrev} accessibilityRole="button">
-          <Icon name="IcBack" size={28} color="#9D9D9D" />
+          <Icon name="IcBack" size={28} color="#808080" />
         </Pressable>
-        <Text className="subHeading1B text-gray-700">{monthKo(month)}</Text>
+        <Text className="subHeading1B text-white">{monthKo(month)}</Text>
         <Pressable onPress={onNext} accessibilityRole="button">
-          <Icon name="IcNext" size={28} color="#9D9D9D" />
+          <Icon name="IcNext" size={28} color="#808080" />
         </Pressable>
       </View>
       <View className="w-full flex-row justify-evenly gap-5 px-3 py-2">
         {DAY_NAMES.map((n, i) => (
           <Text
             key={n}
-            className={`subHeading3 h-6 w-6 text-center ${i === 0 ? 'text-[#CD8383]' : i === 6 ? 'text-[#71ACC9]' : 'text-[#585858]'}`}
+            className={`subHeading3 h-6 w-6 text-center ${i === 0 ? 'text-[#D63D3D]' : i === 6 ? 'text-[#71ACC9]' : 'text-gray-100'}`}
           >
             {n}
           </Text>
