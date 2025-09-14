1037a673a66a009b227677e558046a17825cda04
cjiyun <chlwldbs78@gmail.com>
2025-09-14 13:15:26 +0900

    style: 헤더 완료 버튼 로딩 처리

--- src/components/common/SaveButton.tsx
+++ src/components/common/SaveButton.tsx
@@ -1,18 +1,20 @@
 import { Pressable, Text } from 'react-native';
+import Loader from '@common/Loader';
 
 type SaveButtonProps = {
   onPress?: () => void;
   disabled?: boolean;
+  isLoading?: boolean;
 };
 
-const SaveButton = ({ onPress, disabled }: SaveButtonProps) => {
+const SaveButton = ({ onPress, disabled, isLoading }: SaveButtonProps) => {
   return (
     <Pressable
       onPress={onPress}
-      className={`rounded-lg ${disabled ? 'bg-yellow-100' : 'bg-yellow-200'} px-3 py-2`}
-      disabled={disabled}
+      className={`rounded-lg ${disabled ? 'bg-gray-800' : 'bg-yellow-200'} px-3 py-2`}
+      disabled={disabled || isLoading}
     >
-      <Text className="body2 text-gray-900">{`완료`}</Text>
+      {isLoading ? <Loader /> : <Text className="body2 text-gray-900">{`완료`}</Text>}
     </Pressable>
   );
 };
