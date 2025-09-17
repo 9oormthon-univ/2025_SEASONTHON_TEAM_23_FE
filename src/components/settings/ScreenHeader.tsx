deb13e94619af7661b16420b7cb8d5e9bb89b7c4
cjiyun <chlwldbs78@gmail.com>
2025-09-18 07:03:21 +0900

    style: 불필요한 패딩 제거

--- src/components/settings/ScreenHeader.tsx
+++ src/components/settings/ScreenHeader.tsx
@@ -6,7 +6,7 @@ type ScreenHeaderProps = {
 
 const ScreenHeader = ({ title }: ScreenHeaderProps) => {
   return (
-    <View className="gap-7 pt-5">
+    <View className="gap-7">
       <Text className="heading2B px-7 !leading-[42px] text-white">{title}</Text>
       <View className="h-2 border border-[#4A5263] bg-[#303A4F]"></View>
     </View>
